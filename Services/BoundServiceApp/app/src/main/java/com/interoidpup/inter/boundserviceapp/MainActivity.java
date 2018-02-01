2c173bcfd60489b83d43c5725b0ee8b3e143f5d7
Büşra Serdaroğlu <bakdag@mngkargo.com.tr>
2018-02-01 13:33:58 +0300

    Bound servisi bağlama, çalıştırma ve durdurma örneği

--- Services/BoundServiceApp/app/src/main/java/com/interoidpup/inter/boundserviceapp/MainActivity.java
+++ Services/BoundServiceApp/app/src/main/java/com/interoidpup/inter/boundserviceapp/MainActivity.java
@@ -1,25 +1,54 @@
 package com.interoidpup.inter.boundserviceapp;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = "MainActivity";
     // Bir saniye arayla random String oluşturup ekrana basalım.
     //bu ekranı kitler. Bunun için Service' in onStartCommand' ını thread içerisinde gerçekleştirilebilir.
-
-    Button btnStartBoundService;
-    boolean isStarted = false;
+    Button btnStartBoundService, btnStartService, btnCreateString;
+    //butonların durumunu tutar.
+    boolean isStartedService = true, isStartedBoundService = true;
+    //Servis ile componentin bağlantılı olup olmadığını sürekli kontrol edecek sınıf
+    ServiceConnection serviceConnection;
+    //Bir bağlantı işlemi olduğu için işlem yapmadan önce bağlı olup olmadığımı kontrol etmek için bir değişken
+    private boolean isConnected = false;
+    //Servisten activity' e veri almak için
+    private MyBoundService myBoundService;
+    //*** bind bağlamak, başlatmak demek değil, o yüzden bağlandıktan sonra tekrar çalışması için servisi başlatmalısın !
 
     void init(){
         btnStartBoundService = (Button) findViewById(R.id.btnStartBoundService);
+        btnStartService = (Button) findViewById(R.id.btnStartService);
+        btnCreateString = (Button) findViewById(R.id.btnCreateString);
+
         btnStartBoundService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startBoundService();
+            }
+        });
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService();
+            }
+        });
+        btnCreateString.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getRandomString();
             }
         });
     }
@@ -28,21 +57,94 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         init();
     }
 
-    void startBoundService(){
-        if (isStarted){
-            btnStartBoundService.setText("Bound Servisi Durdur");
-            isStarted  = true;
+    void startService(){
+        if (isStartedService){
+            btnStartService.setText("Servisi Durdur");
+            isStartedService  = false;
             Intent intent = new Intent(MainActivity.this, MyBoundService.class);
             startService(intent);
+            Log.e(TAG, "Servis başladı");
         }else{
-            btnStartBoundService.setText("Bound Servisi Çalıştır");
-            isStarted  = false;
+            btnStartService.setText("Servisi Çalıştır");
+            isStartedService  = true;
             Intent intent = new Intent(MainActivity.this, MyBoundService.class);
             stopService(intent);
+            Log.e(TAG, "Servis durduruldu");
+        }
+    }
+
+    void startBoundService(){
+        if (isStartedBoundService){
+            btnStartBoundService.setText("Bound Servisi Durdur");
+            isStartedBoundService= false;
+            bindMyBoundService();
+        }else{
+            btnStartBoundService.setText("Bound Servisi Çalıştır");
+            isStartedBoundService= true;
+            unbindMyBoundService();
+        }
+    }
+
+    void bindMyBoundService(){
+        //ilk connection' u kontrol et
+        if (serviceConnection == null){
+            serviceConnection = new ServiceConnection() {
+                @Override
+                public void onServiceConnected(ComponentName componentName, IBinder ıBinder) {
+                    isConnected = true;
+                    //Activity servise hala bağlanmadı. Bu metodun parametreleri sayesinde bağlama işlemi gerçekleşecek.
+                    // componentName : bağlı olduğu componentin adı
+                    //ıBinder : serviste tanımladığımız local binder
+                    MyBoundService.MyLocalBinder myLocalBinder  = (MyBoundService.MyLocalBinder) ıBinder;
+                    //şimdi tanımladığımız myLocalBinder sayesinde servise erişelim.
+                    myBoundService = myLocalBinder.getService();
+                    Log.e(TAG, "Bound Service Connected Çağrıldı");
+
+                }
+
+                @Override
+                public void onServiceDisconnected(ComponentName componentName) {
+                    //telefon kaynakları sıkıntılı duruma düşerse o zaman bu metod çalışır.
+                    isConnected = false;
+                    Log.e(TAG, "Bound Service Disconnected Çağrıldı");
+                }
+            };
+        }
+        //servise bağlı değilse intent oluştur ve bindservice() metodunu çalıştır.
+        if (isConnected == false){
+            //bind edelim
+            Intent boundIntent = new Intent(this, MyBoundService.class);
+            //flag olarak BIND_AUTO_CREATE : servise bağlı component varsa oluştur.
+            bindService(boundIntent, serviceConnection, Context.BIND_AUTO_CREATE );
+            isConnected = true;
+            Log.e(TAG, "Bound Servis bağlantısı yapıldı");
+        }else{
+            //zaten bağlı ise toas mesajıyla bildir.
+            Toast.makeText(this, "Bound Servise Zaten Bağlı", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+    void unbindMyBoundService(){
+        if (isConnected == true){
+            unbindService(serviceConnection);
+            isConnected = false;
+            Log.e(TAG, "Bound Servis bağlantısı kesildi");
+        }else{
+            Toast.makeText(this, "Bound Servise zaten bağlı değil", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void getRandomString(){
+        //bu metod servisi bağlayıp başlattıktan sonra random string üretir.
+        if (isConnected == true) {
+            Toast.makeText(this, "Üretilen String :" + myBoundService.getString(), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Önce Bound servis bağlantısını yapın", Toast.LENGTH_SHORT).show();
         }
     }
 
--- Services/BoundServiceApp/app/src/main/java/com/interoidpup/inter/boundserviceapp/MyBoundService.java
+++ Services/BoundServiceApp/app/src/main/java/com/interoidpup/inter/boundserviceapp/MyBoundService.java
@@ -2,6 +2,7 @@ package com.interoidpup.inter.boundserviceapp;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -17,41 +18,72 @@ public class MyBoundService extends Service {
     // UI kitlenir. Çünkü Main Threadde çalışır.
     boolean isContinue = true;
     private String TAG = "MyBoundService";
+    String randomStr = "";
+
+    //onBind için inner class oluşturalım. Local : uygulama iç, Binder' den extends etmemizin sebebi
+    // IBinder interface extends edemeyiz, onu implement eden sınıf Binder. IBinder -> Binder -> MyLocalBinder
+    class MyLocalBinder extends Binder {
+        //Activity' den service bağlanırken servise ihtiyacımız olacak.
+        public MyBoundService getService(){
+            return MyBoundService.this;
+        }
+    }
+    //Yukarıda ürettiğimiz sınıf tipinde bir değişken oluşturalım.
+    private IBinder myLocalBinder = new MyLocalBinder();
+
+    @Nullable
+    @Override //IBinder artık null değil flag dönecek.
+    public IBinder onBind(Intent ıntent) {
+        Log.e(TAG, "onBind");
+        return myLocalBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.e(TAG, "onUnbind");
+        return false;
+    }
 
     @Override
     public void onCreate() {
-        Log.i(TAG, "onCreate");
+        Log.e(TAG, "onCreate");
         super.onCreate();
     }
 
     @Override
     public void onDestroy() {
-        Log.i(TAG, "onDestroy");
+        Log.e(TAG, "onDestroy");
         isContinue = false;
         super.onDestroy();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i(TAG, "onStartCommand");
+        Log.e(TAG, "onStartCommand");
 
+        //farklı threadlarda oluşacağı için UI kitlenmeyecek.
         new Thread(new Runnable() {
             @Override
             public void run() {
-                //farklı bir thread' e taşıdık. Kilitlenmeyi engelledik.
-                while (isContinue){
-                    String randomStr = UUID.randomUUID().toString();
-                    Log.i(TAG, randomStr);
-                }
+                createString();
             }
-        });
+        }).start();
         return START_STICKY;
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent ıntent) {
-        Log.i(TAG, "onBind");
-        return null;
+    void createString(){
+        while (isContinue){
+            try{
+                Thread.sleep(1000);
+                randomStr = UUID.randomUUID().toString();
+                Log.e(TAG, randomStr);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getString(){
+        return randomStr;
     }
 }
