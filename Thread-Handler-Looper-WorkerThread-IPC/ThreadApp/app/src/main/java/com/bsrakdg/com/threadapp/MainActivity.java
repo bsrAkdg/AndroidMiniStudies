46a0384ad4451d3377d7e09169365c300226cab7
bsrAkdg <busraaakdag@gmail.com>
2018-02-01 16:58:05 +0300

    UI Kitlenmesi custom Thread ile ya da normal Thread ile önlendi

--- Thread-Handler-Looper-WorkerThread-IPC/ThreadApp/app/src/main/java/com/bsrakdg/com/threadapp/MainActivity.java
+++ Thread-Handler-Looper-WorkerThread-IPC/ThreadApp/app/src/main/java/com/bsrakdg/com/threadapp/MainActivity.java
@@ -9,12 +9,13 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     //UI' yı(main thread), uzun sürecek bir döngü ile kitliyoruz. Bu yüzden ekrandaki hiçbir viewe erişelemiyor.
-    //çözüm :
+    //çözüm 1 : farklı thread üzerine taşımak :  MyThread class türünde nesneyi onCreate() de oluşturup başlayacağı zaman o thread.start() diyerek çağırmak
 
     private String TAG = "MainActivity";
     Button btnStart, btnStop;
     int loopCount = 0;
     boolean isExecute = true;
+    Thread thread;
 
     void init(){
         btnStart = (Button) findViewById(R.id.btnStart);
@@ -36,23 +37,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
-    }
 
-    void startLoop (){
-        isExecute= true;
-        while (isExecute){
-            try {
-                Thread.sleep(1000);
-                Log.e(TAG, "Döndü sayısı : " + loopCount + " Thread : " + Thread.currentThread().getName());
-                loopCount++;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        //uzun sürecek işlemi başka bir threade taşıdık.
+        MyThread myThread = new MyThread();
+        thread = new Thread(myThread);
+
+        /* TODO diğer yöntem
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                isExecute= true;
+                while (isExecute){
+                    try {
+                        Thread.sleep(1000);
+                        Log.e(TAG, "Döndü sayısı : " + loopCount + " Thread : " + Thread.currentThread().getName());
+                        loopCount++;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
+        }).start();
+        */
     }
-
+    void startLoop (){
+        thread.start(); // MyThread classında run() metodu çalışır.
+    }
     void stopLoop(){
         isExecute= false;
+    }
+    //thread içinde direk new Runnable demek yerine inner class oluşturuyoruz.
+    public class MyThread implements Runnable{
+        //buraya yazdığımız kodlar artık main threadde değil worker threadde çalışır.
+        @Override
+        public void run() {
+            isExecute= true;
+            while (isExecute){
+                try {
+                    Thread.sleep(1000);
+                    Log.e(TAG, "Döndü sayısı : " + loopCount + " Thread : " + Thread.currentThread().getName());
+                    loopCount++;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
