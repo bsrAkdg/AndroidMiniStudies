2992416fcaa18ba7ed41e653b2ff1c907c02c971
bsrAkdg <busraaakdag@gmail.com>
2018-02-02 10:42:42 +0300

    Worker thread ile main thread arasında gönderceğimiz data' yı custom thread ve Message kullanarak gönderme işlemi - çözüm 2

--- Thread-Handler-Looper-WorkerThread-IPC/HandlerApp/app/src/main/java/com/bsrakdg/com/handlerapp/MainActivity.java
+++ Thread-Handler-Looper-WorkerThread-IPC/HandlerApp/app/src/main/java/com/bsrakdg/com/handlerapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bsrakdg.com.handlerapp;
 
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     int loopCount = 0;
     boolean isExecute = true;
     Handler handler; //mainactivity' nin onCreate()' ında tanımlanır.
+    Thread thread;
 
     void init(){
         btnStart = (Button) findViewById(R.id.btnStart);
@@ -48,11 +50,21 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        //custom thread' i create ediyorum.
+        thread = new Thread(new MyThread());
+
         //main threadde handler' ı tanımlıyoruz böylelikle main threade ve onun mesaj kuyruğuna erişebilir.
-        handler = new Handler();
+        handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                //custom threadde sendMessage() dedikten sonra bu metoda düşer.Artık worker thread ile main thread arası iletişim sağlandı.
+                txtLoop.setText("Döngü sayısı : " + msg.arg1 );
+            }
+        };
     }
     void startLoop (){
 
+    /* TODO ilk çözüm : default bir thread kullanmak : onCreate' de bir handle oluştur sonra buraya yönlendir.
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -76,10 +88,40 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
-        }).start();
+        }).start();*/
+
+    // TODO ikinci çözüm : custom thread oluşturmak
+        thread.start();
     }
     void stopLoop(){
         isExecute= false;
+    }
+
+    private class MyThread implements Runnable{
+
+        @Override
+        public void run() {
+            isExecute= true;
+            while (isExecute){
+                try {
+                    Thread.sleep(1000);
+                    Log.e(TAG, "Döngü sayısı : " + loopCount + " Thread : " + Thread.currentThread().getName());
+
+                    //ikinci olarak message gönderelim.
+                    Message message = Message.obtain(); //new message yerine message.obtain() diyoruz çünkü tekrar tekrar oluşturmanın önüne geçer
+                    //Bundle yada arg1,arg2 gibi bir iki elemanlı değer gönderebiliriz.
+                    message.arg1 = loopCount;
+                    message.arg2 = loopCount;
+                    //artık ilgili handler' ın handleMessage() metoduna gelir.
+                    handler.sendMessage(message);
+                    //handler.sendMessageDelayed(message, 5000); 5 sn sonra iletir.
+                    loopCount++;
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
