181d3dfa6945a87ae3d13a56cf7b52f3adfd8901
bsrAkdg <busraaakdag@gmail.com>
2018-02-02 08:59:38 +0300

    bir worker thread ile main thread(UI) arasında Runnable nesnesi ile iletişim kurma

--- Thread-Handler-Looper-WorkerThread-IPC/ThreadApp/app/src/main/java/com/bsrakdg/com/threadapp/MainActivity.java
+++ Thread-Handler-Looper-WorkerThread-IPC/ThreadApp/app/src/main/java/com/bsrakdg/com/threadapp/MainActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //UI' yı(main thread), uzun sürecek bir döngü ile kitliyoruz. Bu yüzden ekrandaki hiçbir viewe erişelemiyor.
-    //çözüm 1 : farklı thread üzerine taşımak :  MyThread class türünde nesneyi onCreate() de oluşturup başlayacağı zaman o thread.start() diyerek çağırmak
-
+    //çözüm 1 : custom thread ile farklı thread üzerine taşımak :  MyThread class türünde nesneyi onCreate() de oluşturup başlayacağı zaman o thread.start() diyerek çağırmak
+    //çözüm 2 new thread ile farklı thread üzerine taşımak
     private String TAG = "MainActivity";
     Button btnStart, btnStop;
+    TextView txtLoop;
     int loopCount = 0;
     boolean isExecute = true;
     Thread thread;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     void init(){
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStop = (Button) findViewById(R.id.btnStop);
+        txtLoop = (TextView)findViewById(R.id.txtLoop);
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,11 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
-        //uzun sürecek işlemi başka bir threade taşıdık.
+        /* TODO çözüm 1
         MyThread myThread = new MyThread();
-        thread = new Thread(myThread);
+        thread = new Thread(myThread);*/
 
-        /* TODO diğer yöntem
+    }
+    void startLoop (){
+        /* TODO çözüm 1
+        thread.start(); // MyThread classında run() metodu çalışır.*/
+
+        // TODO çözüm 2
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -54,21 +64,41 @@ public class MainActivity extends AppCompatActivity {
                         Thread.sleep(1000);
                         Log.e(TAG, "Döndü sayısı : " + loopCount + " Thread : " + Thread.currentThread().getName());
                         loopCount++;
+                        //todo bir worker threadden main thread' e erişebilmek için üç yöntem var :
+                        //bu threadden Toast mesajı basmak istersek UI thread' e erişmek için aşağıdakilerden bir tanesini kullanmak yeterli.
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, ""+ loopCount, Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        //bu threadden UI daki textviewi güncellemek istersek
+                        txtLoop.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                //runnable nesnesiyle main threade erişebilirsin.
+                                txtLoop.setText(""+loopCount);
+                            }
+                        });
+                        //ilgili view öğesinin postDelay metodunuda kullanabiliriz:
+                        txtLoop.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+
+                            }
+                        }, 5000); //5 sn sonra bu işlemleri yapacak.
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
         }).start();
-        */
-    }
-    void startLoop (){
-        thread.start(); // MyThread classında run() metodu çalışır.
     }
     void stopLoop(){
         isExecute= false;
     }
-    //thread içinde direk new Runnable demek yerine inner class oluşturuyoruz.
+
+    //TODO çözüm 1 : thread içinde direk new Runnable demek yerine inner class oluşturuyoruz.
     public class MyThread implements Runnable{
         //buraya yazdığımız kodlar artık main threadde değil worker threadde çalışır.
         @Override
